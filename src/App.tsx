5a391bb039dc493473b93dcd77dd36f10dd00693
FrontEndDevo <mylinkacco@gmail.com>
2024-07-28 15:46:33 +0300

    Remove unused code and add a title for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Button, ButtonGroup, Container } from "@mui/material";
+
 function App() {
-  return <h1>Hello from Typescript</h1>;
+  return (
+    <Container maxWidth="sm">
+      <ButtonGroup variant="contained" aria-label="Basic button group">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+    </Container>
+  );
 }
 
 export default App;
